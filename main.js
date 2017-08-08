c490cc309c870b669bce2a444c58ed40f763c265
liuxiaoran <mk9540@163.com>
2017-08-08 13:11:16 +0800

    normal

---
+++ main.js
@@ -0,0 +1,11 @@
+var fs = require('fs');
+fs.readFile('sample.txt', 'utf-8', function (err, data) {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log(data);
+    }
+});
+console.log(22);
+//# sourceMappingURL=main.js.map
