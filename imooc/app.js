580b25e38d1a87aaa4bcef8f4e877841bcf7ae14
liuxiaoran <mk9540@163.com>
2018-03-16 11:50:12 +0800

    详情页面去除循环

--- imooc/app.js
+++ imooc/app.js
@@ -66,7 +66,7 @@ app.get('/admin/list',function(req,res){
 app.get('/detail/:id',function(req,res){
     res.render('detail',{
         title: 'imooc detailPage',
-        movies: [{
+        movie:{
             doctor:'刘小然',
             country:'天朝',
             title:'哈哈哈',
@@ -75,8 +75,7 @@ app.get('/detail/:id',function(req,res){
             language:'英语',
             video:'http://vt1.doubanio.com/201803131120/f4a3b0291d3606bbae91158be210d24b/view/movie/M/302260016.mp4',
             summary: '本片以习近平新时代中国特色社会主义思想为内在逻辑，展示了在创新、协调、绿色、开放和共享的新发展理念下中国这五年的伟大成就，展现了中国人民在全面建设小康征程上的伟大奋斗，彰显了以习近平同志为核心的党中央的正确领导。凝聚起全党全国人民的磅礴力量，为实现中华民族伟大复兴的中国梦不断前进。 '
-
-        }]
+        }
     })
 })
 app.get('/admin/movie',function(req,res){
